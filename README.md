528f89d47c03a291c00ae4a74b414f0f2c4dc5e5
jonashartwig <nemuteph@gmx.de>
2017-11-27 11:25:31 +0100

    removed intermediate files

---
+++ README.md
@@ -0,0 +1,4 @@
+required to set:
+
+LIBRARY_PATH to have libkenlm and libkenlm_util
+CPATH to point to kenlm source, JAVA_HOME/include and JAVA_HOME/include/<arch>
--- build.sh
+++ build.sh
@@ -1,37 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-ARCH=$(uname -m |sed -e 's/x86_64/amd64/g')
-OS=$(uname -o |sed -e 's/GNU\/Linux/Linux/g')
 JNIPATH="./src/main/java/com/github/jbaiter/kenlm/jni"
-SRCPATH="./src/main/c++"
-LIBPATH="./src/main/resources/lib/$OS/$ARCH"
 SWIGPATH="./src/main/swig/KenLM.swg"
-LIBNAME="libkenlm.so"
 
-echo Generating source files with SWIG
-rm -f $JNIPATH/*.java $SRCPATH/kenlm_wrap.cc
 swig -c++ -java -Wall -package com.github.jbaiter.kenlm.jni \
-     -outdir $JNIPATH -o $SRCPATH/kenlm_wrap.cc -I$SRCPATH $SWIGPATH
+     -outdir $JNIPATH -o kenlm_wrap.cc $SWIGPATH
 
+CXXFLAGS="-I. -O3 -DNDEBUG -DHAVE_BZLIB -DKENLM_MAX_ORDER=6 -fPIC $CXXFLAGS"
 
-echo Compiling source files
-
-rm -f $SRCPATH/{lm,util}/*.o
-
-CXXFLAGS="-I. -O3 -DNDEBUG -DHAVE_ZLIB -DHAVE_BZLIB -DHAVE_XZLIB -DKENLM_MAX_ORDER=6 -fPIC $CXXFLAGS"
-
-#Grab all cc files in these directories except those ending in test.cc or main.cc
-objects=""
-for i in $SRCPATH/util/double-conversion/*.cc $SRCPATH/util/*.cc $SRCPATH/lm/*.cc; do
-    g++ -I$SRCPATH $CXXFLAGS -c $i -o ${i%.cc}.o
-    objects="$objects ${i%.cc}.o"
-done
-
-echo Compiling JNI library to $LIBPATH/$LIBNAME
-g++ $CXXFLAGS $SRCPATH/kenlm_wrap.cc \
-    -I$JAVA_HOME/include \
-    -I$JAVA_HOME/include/linux \
-    -I$SRCPATH \
-    $objects -shared -Wl,-soname,$LIBNAME \
-    -o $LIBPATH/$LIBNAME -lz -Wno-deprecated -pthread
+g++ $CXXFLAGS ./kenlm_wrap.cc \
+    $objects -shared -lkenlm -lkenlm_util \
+    -o ./libkenlm-jni -Wno-deprecated -pthread
